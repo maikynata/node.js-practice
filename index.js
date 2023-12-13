724a2f400bc76561110959e93ee6ee0de8627d0d
maikynata <maikynata25@gmail.com>
2023-12-13 14:41:52 -0300

    npm init and express install

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
+const express =  require('express');
+const { request } = require('http');
+
+const app = express();
+
+app.get('/', (request, response) => {
+    
+
+});
+
 // const { readFile, readFileSync } = require('fs');
 
 // const txt =  readFileSync ('./hello.txt', 'utf8');
@@ -6,8 +16,8 @@
 // console.log(txt)
 // console.log('Run ASAP')
 
-const { readFile } = require ('fs').promisses;
+// const { readFile } = require ('fs').promisses;
 
-async function hello() {
-    const file = await readFile('./hello.txt', 'utf8');
-}
+// async function hello() {
+//     const file = await readFile('./hello.txt', 'utf8');
+// }
