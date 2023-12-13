552a48778c80b8a308899dc219f2bc9502899b9e
maikynata <maikynata25@gmail.com>
2023-12-13 15:00:46 -0300

    app async get

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 
 const express =  require('express');
+const { readFile } = require('fs');
 const { request } = require('http');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    
+// app.get('/', (request, response) => {
+
+//     readFile('./home.html', 'utf8', (err, html) => {
+
+//         if (err){
+//             response.status(500).send('sorry, server with error')
+//         }
+
+//         response.send(html);
+
+//     })
+
+// });
+
+
+// get with promisses
+app.get('/', async (request, response) => {
+
+    response.send( await readFile('./home.html', 'utf8'));
 
 });
+
+app.listen(process.env.PORT || 3000, () => console.log(' Node App Availabe on http://localhost:3000'))
 
 // const { readFile, readFileSync } = require('fs');
 
