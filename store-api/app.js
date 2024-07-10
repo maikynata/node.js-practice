1f74c742c5ffa864887d6b8b832478780ec5e85d
maikynata <maikynata25@gmail.com>
2024-07-10 23:14:07 +0200

    fixing deprecated mongo package

--- store-api/app.js
+++ store-api/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect');
 
 const notFoundMiddlware =  require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -25,6 +26,7 @@ const port = process.env.PORT || 3000
 const start = async () => {
     try {
         // connect DB
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on port ${port}...`))
     } catch (error) {
         console.log(error)
