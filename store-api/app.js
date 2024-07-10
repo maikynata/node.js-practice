7ff1c0f03f4e5eb05e6d236f9ee8f4191c543987
maikynata <maikynata25@gmail.com>
2024-07-10 22:08:50 +0200

    starting the app

--- store-api/app.js
+++ store-api/app.js
@@ -1,1 +1,21 @@
-console.log('04 Store API')
+require('dotenv').config()
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddlware =  require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+// products route
+
+
+app.use(notFoundMiddlware)
+app.use(errorMiddleware)
